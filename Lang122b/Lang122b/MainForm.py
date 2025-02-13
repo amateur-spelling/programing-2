f6ff851351a918f983aa6f61c851e03ba6ef13ad
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2025-02-13 15:28:55 -0600

    2/13

--- Lang122b/Lang122b/MainForm.py
+++ Lang122b/Lang122b/MainForm.py
@@ -17,6 +17,7 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
+		self._listBox1.BackColor = System.Drawing.Color.Salmon
 		self._listBox1.FormattingEnabled = True
 		self._listBox1.Location = System.Drawing.Point(13, 13)
 		self._listBox1.Name = "listBox1"
@@ -25,35 +26,43 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
+		self._button1.BackColor = System.Drawing.Color.OrangeRed
+		self._button1.ForeColor = System.Drawing.Color.Indigo
 		self._button1.Location = System.Drawing.Point(182, 13)
 		self._button1.Name = "button1"
 		self._button1.Size = System.Drawing.Size(90, 43)
 		self._button1.TabIndex = 1
 		self._button1.Text = "Print"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
+		self._button2.BackColor = System.Drawing.Color.OrangeRed
+		self._button2.ForeColor = System.Drawing.Color.Indigo
 		self._button2.Location = System.Drawing.Point(182, 62)
 		self._button2.Name = "button2"
 		self._button2.Size = System.Drawing.Size(90, 43)
 		self._button2.TabIndex = 2
 		self._button2.Text = "Clear"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
+		self._button3.BackColor = System.Drawing.Color.OrangeRed
+		self._button3.ForeColor = System.Drawing.Color.Indigo
 		self._button3.Location = System.Drawing.Point(181, 111)
 		self._button3.Name = "button3"
 		self._button3.Size = System.Drawing.Size(90, 43)
 		self._button3.TabIndex = 3
 		self._button3.Text = "Exit"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
+		self.BackColor = System.Drawing.Color.Tomato
 		self.ClientSize = System.Drawing.Size(284, 261)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
@@ -65,8 +74,14 @@ class MainForm(Form):
 
 
 	def Button2Click(self, sender, e):
-		listBox.Items.Clear()
+		self._listBox1.Items.Clear()
 
 	def Button1Click(self, sender, e):
-		listBox.Items.Clear()
+		self._listBox1.Items.Clear()
+		for lcv in range(0,41,1):
+			line = lcv * 4.00
+			self._listBox1.Items.Add(str(lcv) + "\t" + str(line))			
 		
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
