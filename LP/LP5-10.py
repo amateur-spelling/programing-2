4afe041ec7c5bdff8e5097ead04db84b9a9fa1c5
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2025-02-24 21:28:45 +0000

    2/24a

--- LP/LP5-10.py
+++ LP/LP5-10.py
@@ -1,7 +1,9 @@
 num1 = int(input("Enter num: "))
 num2 = int(input("Enter second num: "))
+temp = 0
 while (num2 > 0) {
-    temp = num1 % num2
+    temp = num1 % num2;
     num1 = num2;
     num2 = temp;
 }
+print("The GCD is: " + str(temp))
