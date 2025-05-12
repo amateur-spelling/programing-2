3dd91c109d5f8e9f749ead831fb5de47a8013f7b
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2025-05-12 15:07:14 -0500

    5/12b

---
+++ CSharp/Stringint7/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter a string: ");
+string word = Console.ReadLine().ToLower();
+
+int v = 0;
+int c = 0; 
+
+for (int i = 0; i < word.Length; i++)
+{
+    char letter = word[i];
+    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') v++;
+    else if (letter >= 'a' && letter <= 'z') c++;
+
+}
+Console.WriteLine("{0} contains {1} vowels and {2} consonants", word, v, c);
+Console.ReadLine();
