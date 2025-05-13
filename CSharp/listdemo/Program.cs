b500b3bd3b0ca7e0df181dcc7002742d10d8460e
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2025-05-13 15:09:25 -0500

    5/13

---
+++ CSharp/listdemo/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> nums = new List<int>() { 1, 2, 3 };
+nums.Add(2);
+nums.Add(7);
+//List<TYPE> name = new List<TYPE>();
+nums.Remove(7);
+Console.WriteLine("Length: " + nums.Count);
+Console.WriteLine(String.Join(", ", nums));
+
+foreach (int n in nums)
+    Console.WriteLine(n); 
