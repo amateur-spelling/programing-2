6c149c2faf415644e02e183abf8a29c76d468d24
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2025-05-13 20:26:32 +0000

    5/13a

--- Prog/prog 54b/Prog209A/cl209a.py
+++ Prog/prog 54b/Prog209A/cl209a.py
@@ -1,6 +1,12 @@
 class number: 
-    def __int__(self, ):
-         
+    def __int__(self, value):
+        self.value = value
+        self.morel = 0
+        self.less = 0
         
     def calc(self):
-        if 
+        if value >= 500:
+            self.morel += 1
+        else:
+            self.less += 1 
+        
